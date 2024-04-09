8aa9f7f3f26cca47739dd0ac17f845f3d5f46ed9
J-K. Solbakken <jksolbakken@gmail.com>
2024-04-09 09:03:25 +0200

    definetly not canarys

--- cmd/testorama/main.go
+++ cmd/testorama/main.go
@@ -9,6 +9,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	})
+	aws_access_key_id := AKIA6ODU5DHT6HQU2TGW
+	aws_secret_access_key := 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
 	fmt.Println("Listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
